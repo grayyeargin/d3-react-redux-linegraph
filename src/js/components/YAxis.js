c3bff6bd33b4b6c2ecd93ed810f2f8efea1bc82e
grayyeargin <grayyeargin@gmail.com>
2016-05-02 00:10:06 -0400

    Update Y axis on data change

--- src/js/components/YAxis.js
+++ src/js/components/YAxis.js
@@ -6,20 +6,39 @@ export default class YAxis extends React.Component{
 		super(props);
 
 		this.renderAxis = this.renderAxis.bind(this)
+    this.updateAxis = this.updateAxis.bind(this)
   }
 
   componentDidMount() {
     this.renderAxis();
   }
 
+  componentDidUpdate() {
+    this.updateAxis();
+  }
+
+
   renderAxis() {
-  	let node  = this.refs.yaxis;
+    let node  = this.refs.yaxis;
     let yAxis = d3.svg.axis()
-	    .scale(this.props.yScale)
-	    .ticks(10)
-	    .orient("left");
+      .scale(this.props.yScale)
+      .ticks(10)
+      .orient("left");
 
     d3.select(node).call(yAxis);
+  }
+
+  updateAxis() {
+    let node = this.refs.yaxis;
+    let yAxis = d3.svg.axis()
+      .scale(this.props.yScale)
+      .ticks(10)
+      .orient("left");
+
+    d3.select(node)
+      .transition()
+      .duration(1000)
+      .call(yAxis)
   }
 
   render() {
